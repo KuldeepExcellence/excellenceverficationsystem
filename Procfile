83998a3f77c93c37baac4d6134eb8af7c0d9f41c
Kuldeep Sharma <kuldeep.excellence@gmail.com>
2022-09-17 12:30:16 +0530

    changes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn excellenceapp.wsgi --log-file -
--- verification/settings.py
+++ verification/settings.py
@@ -52,11 +52,13 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+WHITENOISE_USE_FINDERS = True
 
 ROOT_URLCONF = 'verification.urls'
 
